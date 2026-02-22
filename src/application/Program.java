b1ac443028c60e0e25f03d5b0361309e8b23b7e7
William Nogueira <wyllyam11@hotmail.com>
2026-02-22 15:14:43 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,21 +1,16 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		Department dep = new Department(1, "Computer");
-		
-		Seller seller = new Seller(1, "William", "william@hotmail.com", new Date(), 3500.0, dep);
-		
 		SellerDao sellerDao = DaoFactory.crateSellerDao();
+		
+		Seller seller = sellerDao.findById(3);
 		
 		System.out.println(seller);
 
