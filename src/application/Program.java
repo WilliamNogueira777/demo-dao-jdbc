e1d06ff7c0333c62b189f6d39b300367312afe93
William Nogueira <wyllyam11@hotmail.com>
2026-02-22 13:47:12 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -12,6 +14,8 @@ public class Program {
 		Department dep = new Department(1, "Computer");
 		
 		Seller seller = new Seller(1, "William", "william@hotmail.com", new Date(), 3500.0, dep);
+		
+		SellerDao sellerDao = DaoFactory.crateSellerDao();
 		
 		System.out.println(seller);
 
